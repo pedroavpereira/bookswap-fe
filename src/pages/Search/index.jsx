5ceeb60aaa33e434259b487d13d723d419c7568c
Pedro pereira <pedroavpereira2000@gmail.com>
2024-08-28 14:05:01 +0100

    Implemented fetch functionality for search page

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,5 +1,60 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import SearchForm from "../../components/SearchForm";
+import BookList from "../../components/BookList";
+import FullPageSpinner from "../../components/FullPageSpinner";
+
+const API_URL = "http://127.0.0.1:3001";
+
 function Search() {
-  return <div></div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const [collections, setCollections] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  const title = searchParams.get("title");
+  const radius = searchParams.get("radius");
+
+  useEffect(
+    function () {
+      async function fetchCollections() {
+        try {
+          setIsLoading(true);
+          const response = await fetch(
+            `${API_URL}/collections/search?radius=${radius}&lat=${lat}&lng=${lng}&title=${title}`
+          );
+          if (response.ok) {
+            const data = await response.json();
+
+            setCollections(data);
+          } else {
+            throw new Error("err");
+          }
+        } catch (err) {
+          navigate("/");
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      fetchCollections();
+    },
+    [lat, lng, title, radius]
+  );
+
+  if (isLoading) return <FullPageSpinner />;
+
+  return (
+    <div>
+      <SearchForm />
+      <h2>
+        Results for {title} in {radius} miles
+      </h2>
+      <BookList collections={collections} />
+    </div>
+  );
 }
 
 export default Search;
