7a4776bb4d27cb4cb060909cac10d880c07cf278
Pedro pereira <pedroavpereira2000@gmail.com>
2024-08-28 15:04:33 +0100

    Add fetch for rooms

--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -1,4 +1,54 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import FullPageSpinner from "../../components/FullPageSpinner";
+
+const API_URL = "127.0.0.1:3001";
+
 function Chat() {
+  const navigate = useNavigate();
+  const [rooms, setRooms] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(
+    function () {
+      async function fetchRooms() {
+        try {
+          if (!localStorage.getItem("token")) {
+            navigate("/");
+          }
+
+          setIsLoading(true);
+
+          const options = {
+            method: "GET",
+            headers: {
+              Authorization: localStorage.getItem("token"),
+            },
+          };
+          const response = await fetch(`${API_URL}/rooms/`, options);
+
+          if (!response.ok) throw "error";
+
+          const data = await response.json();
+
+          setRooms(data);
+        } catch (err) {
+          console.error(err);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      fetchRooms();
+    },
+    [setRooms, navigate]
+  );
+
+  console.log(rooms);
+
+  if (isLoading) return <FullPageSpinner />;
+
   return <div>Chat</div>;
 }
 
