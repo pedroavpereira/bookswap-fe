6b84ea21cf085e5451ba4bc24efd2743bc4b342c
tiffjai <tiffanieho@outlook.com>
2024-08-29 15:17:19 +0100

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header/index";
 import * as Pages from "./pages";
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<Pages.HomePage />} />
-      <Route path="/login" element={<Pages.Login />} />
-      <Route path="/signup" element={<Pages.Register />} />
-      <Route path="/search" element={<Pages.Search />} />
-      <Route path="/chat" element={<Pages.Chat />} />
-      <Route path="/offering" element={<Pages.Offering />} />
-      <Route path="/swap">
-        <Route index element={<Pages.Swap />} />
-        <Route path="history" element={<Pages.SwapHistory />} />
-      </Route>
-      <Route path="/collection">
-        <Route index element={<Pages.Collection />} />
-        <Route path="add" element={<Pages.CollectionAdd />} />
-      </Route>
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route index element={<Pages.HomePage />} />
+        <Route path="/login" element={<Pages.Login />} />
+        <Route path="/signup" element={<Pages.Register />} />
+        <Route path="/search" element={<Pages.Search />} />
+        <Route path="/chat" element={<Pages.Chat />} />
+        <Route path="/offering" element={<Pages.Offering />} />
+        <Route path="/swap">
+          <Route index element={<Pages.Swap />} />
+          <Route path="history" element={<Pages.SwapHistory />} />
+        </Route>
+        <Route path="/collection">
+          <Route index element={<Pages.Collection />} />
+          <Route path="add" element={<Pages.CollectionAdd />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
