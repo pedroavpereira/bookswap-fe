0348b5d70d7055636e1adfd4d33e615d25c2f0b9
Pedro pereira <pedroavpereira2000@gmail.com>
2024-09-02 09:18:13 +0100

    Update Url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const REGISTER_API_URL = "/signup";
 
-export const AUTH_URL = "http://52.209.137.209:5000";
+export const AUTH_URL = "http://34.242.65.93:5000";
 
-export const API_URL = "http://52.209.137.209:3000";
+export const API_URL = "http://34.242.65.93:3000";
