f3c8c497acf084370a3dda0dd709f7b34914b3d1
Pedro pereira <pedroavpereira2000@gmail.com>
2024-09-02 20:02:47 +0100

    Redesign chat

--- src/components/ChatRoom/index.jsx
+++ src/components/ChatRoom/index.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { FaCircleUser } from "react-icons/fa6";
 import { useUser } from "../../contexts/UserContext";
 import "./ChatRoom.css";
 
@@ -7,24 +8,27 @@ function ChatRoom({ room, onClick }) {
   // console.log(room, room?.last_message, room?.last_message?.read);
   const thereIsMessage = Boolean(room.last_message);
 
+  const messageUnread =
+    thereIsMessage &&
+    !room?.last_message?.read &&
+    room?.last_message.user_sent !== user.user_id;
+
   return (
-    <div
-      className={`chat-room-flex ${room?.last_message && "message"}${
-        room?.last_message?.read ? "message-read" : "message-unread"
-      }`}
-      onClick={() => onClick(room.room_id)}
-    >
-      <p className="chat-room-user">
-        {room.user.first_name} {room.user.last_name}
-      </p>
-      <p className="chat-room-message">
-        {room.last_message && room.last_message.message}
-      </p>
-      {thereIsMessage &&
-        !room?.last_message?.read &&
-        room?.last_message.user_sent !== user.user_id && (
-          <span className="message-unread"></span>
-        )}
+    <div className="chat-row" onClick={() => onClick(room.room_id)}>
+      <FaCircleUser className="chat-row-user" />
+      <div
+        className={`${room?.last_message && "message"} ${
+          messageUnread ? "message-unread" : ""
+        } chat-room-flex`}
+      >
+        <p className="chat-room-user">
+          {room.user.first_name} {room.user.last_name}
+        </p>
+        <p className="chat-room-message">
+          {room.last_message && room.last_message.message}
+        </p>
+      </div>
+      {messageUnread && <span className="unread-icon"></span>}
     </div>
   );
 }
