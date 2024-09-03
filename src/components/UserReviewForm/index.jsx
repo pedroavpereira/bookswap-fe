866a2e546a24ab642249728371c2700e31fd9598
Pedro pereira <pedroavpereira2000@gmail.com>
2024-09-03 14:39:13 +0100

    Add user review

---
+++ src/components/UserReviewForm/index.jsx
@@ -0,0 +1,95 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { Form } from "react-bootstrap";
+
+import { useUser } from "../../contexts/UserContext";
+import { API_URL } from "../../utils/constants";
+
+import StarRating from "../StarRating";
+
+function UserReviewForm({ onSubmit, swap }) {
+  const { user } = useUser();
+  const [rating, setRating] = useState(0);
+  const [message, setMessage] = useState("");
+  const [isReviewing, setIsReviewing] = useState(false);
+
+  const userReviewId =
+    swap.user_offered === user.user_id
+      ? swap.user_requesting
+      : swap.user_offered;
+
+  async function createReview() {
+    const token = localStorage.getItem("token");
+
+    if (!token) return null;
+
+    const reqBody = {
+      rating,
+      message,
+      submitted_by: user.user_id,
+      user_id: userReviewId,
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+      body: JSON.stringify(reqBody),
+    };
+
+    try {
+      setIsReviewing(true);
+
+      const response = await fetch(`${API_URL}/reviews/`, options);
+
+      if (response.status !== 201) return null;
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsReviewing(false);
+    }
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setIsReviewing();
+    if (rating) {
+      await createReview();
+    }
+    onSubmit(swap.swap_id);
+  }
+  return (
+    <Form className="review-form" onSubmit={handleSubmit}>
+      {isReviewing && (
+        <div className="spinner-chat-window-background">
+          <div className="spinner"></div>
+        </div>
+      )}
+      <div className="review-group">
+        <Form.Label>Rating</Form.Label>
+        <StarRating
+          onSetRating={setRating}
+          className="review-flex-item-center"
+          rating={rating}
+          setRating={setRating}
+          color="#f5af38"
+        />
+      </div>
+      <div className="review-group">
+        <Form.Label>Aditional Message</Form.Label>
+        <textarea
+          className="review-textarea"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </div>
+      <button className="action-button" onClick={handleSubmit}>
+        Mark as completed
+      </button>
+    </Form>
+  );
+}
+
+export default UserReviewForm;
--- src/pages/Swap/index.jsx
+++ src/pages/Swap/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Tab, Tabs, Row } from "react-bootstrap";
+import { Container, Tab, Tabs, Row, Modal, Button } from "react-bootstrap";
 import { useSwaps } from "../../contexts/SwapsContext";
 import { useUser } from "../../contexts/UserContext";
 
@@ -6,6 +6,8 @@ import FullPageSpinner from "../../components/FullPageSpinner";
 import SwapsCard from "../../components/SwapsCard";
 import BookList from "../../components/BookList";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import UserReviewForm from "../../components/UserReviewForm";
 
 function Swap() {
   const navigate = useNavigate();
@@ -16,6 +18,10 @@ function Swap() {
     completeSwap,
   } = useSwaps();
   const { user, isLoading: userLoading } = useUser();
+  const [modalIsOpen, setIsModalOpen] = useState(false);
+  const [selectedSwapForCompletion, setSelectedSwapForCompletion] = useState(
+    {}
+  );
 
   const loading = swapsLoading || userLoading;
 
@@ -34,82 +40,110 @@ function Swap() {
     navigate(`/swap/accept/${swap_id}`);
   }
 
+  function toggleModal(swap) {
+    setSelectedSwapForCompletion(swap);
+    setIsModalOpen((s) => !s);
+  }
+
+  function handleComplete(swap_id) {
+    completeSwap(swap_id);
+    setIsModalOpen(false);
+  }
+
   if (loading) return <FullPageSpinner />;
 
   return (
-    <Container className="mt-5">
-      <Row>
-        <h1>Your Swaps</h1>
-      </Row>
+    <>
+      <Container className="mt-5">
+        <Row>
+          <h1>Your Swaps</h1>
+        </Row>
 
-      <Row>
-        <Tabs
-          defaultActiveKey="pending"
-          id="fill-tab-example"
-          className="mb-3"
-          fill
-        >
-          <Tab
-            eventKey="pending"
-            title={`Pending (${pendingSwaps ? pendingSwaps.length : 0})`}
+        <Row>
+          <Tabs
+            defaultActiveKey="pending"
+            id="fill-tab-example"
+            className="mb-3"
+            fill
           >
-            <BookList>
-              {pendingSwaps?.map((swap) => (
-                <SwapsCard swap={swap} key={swap.swap_id}>
-                  <div className="swap-action-buttons">
-                    <button
-                      className="action-button action-button-highlight"
-                      onClick={() => navigateToAccept(swap.swap_id)}
-                    >
-                      View collection
-                    </button>
-                    <button
-                      className="action-button action-button-highlight"
-                      onClick={() => rejectSwap(swap.swap_id)}
-                    >
-                      Reject
-                    </button>
-                  </div>
-                </SwapsCard>
-              ))}
-            </BookList>
-          </Tab>
-          <Tab
-            eventKey="accepted"
-            title={`Accepted (${acceptedSwaps ? acceptedSwaps.length : 0})`}
-          >
-            <BookList>
-              {acceptedSwaps?.map((swap) => (
-                <SwapsCard swap={swap} key={swap.swap_id}>
-                  <div className="swap-action-buttons">
-                    <button
-                      className="action-button action-button-highlight"
-                      onClick={() => completeSwap(swap.swap_id)}
-                    >
-                      Mark as completed
-                    </button>
-                  </div>
-                </SwapsCard>
-              ))}
-            </BookList>
-          </Tab>
-          <Tab
-            eventKey="completed"
-            title={`Completed (${completedSwaps ? completedSwaps.length : 0})`}
-          >
-            <BookList>
-              {completedSwaps?.map((swap) => (
-                <SwapsCard swap={swap} key={swap.swap_id}>
-                  <div className="swap-action-buttons">
-                    Swap was: {swap.status}
-                  </div>
-                </SwapsCard>
-              ))}
-            </BookList>
-          </Tab>
-        </Tabs>
-      </Row>
-    </Container>
+            <Tab
+              eventKey="pending"
+              title={`Pending (${pendingSwaps ? pendingSwaps.length : 0})`}
+            >
+              <BookList>
+                {pendingSwaps?.map((swap) => (
+                  <SwapsCard swap={swap} key={swap.swap_id}>
+                    <div className="swap-action-buttons">
+                      <button
+                        className="action-button action-button-highlight"
+                        onClick={() => navigateToAccept(swap.swap_id)}
+                      >
+                        View collection
+                      </button>
+                      <button
+                        className="action-button action-button-highlight"
+                        onClick={() => rejectSwap(swap.swap_id)}
+                      >
+                        Reject
+                      </button>
+                    </div>
+                  </SwapsCard>
+                ))}
+              </BookList>
+            </Tab>
+            <Tab
+              eventKey="accepted"
+              title={`Accepted (${acceptedSwaps ? acceptedSwaps.length : 0})`}
+            >
+              <BookList>
+                {acceptedSwaps?.map((swap) => (
+                  <SwapsCard swap={swap} key={swap.swap_id}>
+                    <div className="swap-action-buttons">
+                      <button
+                        className="action-button action-button-highlight"
+                        onClick={() => toggleModal(swap)}
+                      >
+                        Mark as completed
+                      </button>
+                    </div>
+                  </SwapsCard>
+                ))}
+              </BookList>
+            </Tab>
+            <Tab
+              eventKey="completed"
+              title={`Completed (${
+                completedSwaps ? completedSwaps.length : 0
+              })`}
+            >
+              <BookList>
+                {completedSwaps?.map((swap) => (
+                  <SwapsCard swap={swap} key={swap.swap_id}>
+                    <div className="swap-action-buttons">
+                      Swap was: {swap.status}
+                    </div>
+                  </SwapsCard>
+                ))}
+              </BookList>
+            </Tab>
+          </Tabs>
+        </Row>
+      </Container>
+      {modalIsOpen && (
+        <Modal show={modalIsOpen} onHide={() => toggleModal({})} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Review a fellow book lover</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            <UserReviewForm
+              swap={selectedSwapForCompletion}
+              onSubmit={handleComplete}
+            />
+          </Modal.Body>
+        </Modal>
+      )}
+    </>
   );
 }
 
