f12ff2e335718f32812a4989cd34f9e5ec4b3214
Pedro pereira <pedroavpereira2000@gmail.com>
2024-09-03 21:03:14 +0100

    Add visual tweaks to offering page

--- src/pages/Offering/index.jsx
+++ src/pages/Offering/index.jsx
@@ -126,33 +126,43 @@ const OfferingPage = () => {
                   )}
               </div>
             </div>
-            <div className="product-info">
+            <div className="product-info-container">
               <h2 className="book-title">{bookData.book.title}</h2>
-              <p>
-                <span className="book-attribute">
-                  Author{bookData.book.authors.length > 1 ? "'s " : " "}:
-                </span>
-                {bookData.book.authors.join(", ")}
-              </p>
-              <p>
-                {" "}
-                <span className="book-attribute">Condition: </span>
-                {bookData.condition}
-              </p>
-              <p>
-                <span className="book-attribute">Owner: </span>
-                {userData.first_name} {userData.last_name}
-              </p>
-              <p>
-                <span className="book-attribute">Categories: </span>{" "}
-                {bookData.book.categories.join(", ")}
-              </p>
+              <div className="book-attribute-container">
+                <p className="book-attribute">
+                  <span className="book-attribute-hightlight">
+                    Author{bookData.book.authors.length > 1 ? "'s " : " "}:
+                  </span>
+                  {bookData.book.authors.join(", ")}
+                </p>
+                <p className="book-attribute">
+                  {" "}
+                  <span className="book-attribute-hightlight">Condition: </span>
+                  {bookData.condition}
+                </p>
+                <p className="book-attribute">
+                  <span className="book-attribute-hightlight">Owner: </span>
+                  {userData.first_name} {userData.last_name}
+                </p>
+                <p className="book-attribute">
+                  <span className="book-attribute-hightlight">
+                    Categories:{" "}
+                  </span>{" "}
+                  {bookData.book.categories.join(", ")}
+                </p>
+                <p className="book-swap-preferences">
+                  <span className="book-attribute-hightlight">
+                    Delivery Preference:
+                  </span>{" "}
+                  {bookData.delivery_preference.join(", ")}
+                </p>
+              </div>
 
               <div className="swap-details">
-                <h3>Swap Details</h3>
+                {/* <h3>Swap Details</h3>
                 <p>
                   Delivery Preference: {bookData.delivery_preference.join(", ")}
-                </p>
+                </p> */}
                 {userData.lat && userData.lng && (
                   <MapComponent
                     latitude={userData.lat}
