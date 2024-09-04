d1b48657bd38c24609ec360fb20f2f366df68d1b
Stephanie <tiffanieho@outlook.com>
2024-09-04 13:06:51 +0100

    add HomePage component

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,9 +1,13 @@
-import SearchForm from "../../components/SearchForm";
-
+import React from 'react';
+import SearchForm from '../../components/SearchForm';
+import AnimationBackground from '../../components/AnimationBackground'; // Adjust path if needed
 
 function HomePage() {
   return (
     <div className="homepage-background">
+      {/* Add AnimationBackground component */}
+      <AnimationBackground />
+      
       <div className="homepage-headers-container">
         <h1 className="homepage-header">Book Nest</h1>
         <h2 className="homepage-subheader">Find Books. Swap Stories</h2>
